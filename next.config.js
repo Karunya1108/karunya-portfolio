29e9e87938515c7e03517f66b63127bb956f11b5
Karunya <karunyavenkatachalam11@gmail.com>
2025-07-10 21:41:56 +0530

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
-    styledComponents: true, // ✅ Enables SSR for styled-components
-  },
+    styledComponents: true
+  }
 }
 
-
+module.exports = nextConfig;
